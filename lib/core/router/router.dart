fa31eabe4dbc73decab37a7b5bdb04b40d5e5445
oluwajuwon <juwonayanniran@foodco.ng>
2024-11-19 16:50:56 +0100

    changed paypal package to one that works

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: prefer_typing_uninitialized_variables, unused_import
 
 import 'package:flutter/material.dart';
+import 'package:kiind_web/core/models/donation_info_model.dart';
 import 'package:kiind_web/core/router/route_paths.dart';
 import 'package:kiind_web/features/payment/pages/pay_modal.dart';
 import 'package:kiind_web/features/payment/pages/payment_method_screen.dart';
 import 'package:kiind_web/features/payment/pages/payment_summary/payment_summary_page.dart';
+import 'package:kiind_web/features/philanthropy/presentation/pages/charity_allocation/charity_allocation_page.dart';
+import 'package:kiind_web/features/philanthropy/presentation/pages/kiind_portfolio/empty_portfolio_page.dart';
+import 'package:kiind_web/features/philanthropy/presentation/pages/kiind_portfolio/kiind_portfolio_page.dart';
+import 'package:kiind_web/features/philanthropy/presentation/pages/payment_method/philanthropy_payment_method_page.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -48,26 +53,26 @@ class AppRouter {
       //     settings: settings,
       //   );
 
-      // case RoutePaths.charityAllocationScreen:
-      //   final arg = settings.arguments as Map<String, dynamic>;
+      case RoutePaths.charityAllocationScreen:
+        final arg = settings.arguments as Map<String, dynamic>;
 
-      //   return MaterialPageRoute(
-      //       builder: (_) => CharityAllocationPage(
-      //             selectedCharities: arg['selectedCharities'],
-      //             donationInfo: arg['donationInfo'],
-      //             subscriptionId: arg['subscriptionId'],
-      //           ));
-      // case RoutePaths.kiindPortfolioScreen:
-      //   return MaterialPageRoute(builder: (_) => const KiindPortfolioPage());
-      // case RoutePaths.emptyKiindPortfolioScreen:
-      //   return MaterialPageRoute(
-      //       builder: (_) => const EmptyKiindPortfolioPage());
-      // case RoutePaths.philanthropyPaymentMethodSscreen:
-      //   final arg = settings.arguments as DonationInfoModel;
-      //   return MaterialPageRoute(
-      //       builder: (_) => PhilanthropyPaymentMethodPage(
-      //             donationInfo: arg,
-      //           ));
+        return MaterialPageRoute(
+            builder: (_) => CharityAllocationPage(
+                  selectedCharities: arg['selectedCharities'],
+                  donationInfo: arg['donationInfo'],
+                  subscriptionId: arg['subscriptionId'],
+                ));
+      case RoutePaths.kiindPortfolioScreen:
+        return MaterialPageRoute(builder: (_) => const KiindPortfolioPage());
+      case RoutePaths.emptyKiindPortfolioScreen:
+        return MaterialPageRoute(
+            builder: (_) => const EmptyKiindPortfolioPage());
+      case RoutePaths.philanthropyPaymentMethodSscreen:
+        final arg = settings.arguments as DonationInfoModel;
+        return MaterialPageRoute(
+            builder: (_) => PhilanthropyPaymentMethodPage(
+                  donationInfo: arg,
+                ));
 
       default:
         return MaterialPageRoute(
