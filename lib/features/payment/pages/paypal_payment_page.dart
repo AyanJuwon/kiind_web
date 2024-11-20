848d323aef8571e3c3386af3e133c4f56e4d20eb
oluwajuwon <juwonayanniran@foodco.ng>
2024-11-20 18:19:39 +0100

    switch t paypal web sdk

--- lib/features/payment/pages/paypal_payment_page.dart
+++ lib/features/payment/pages/paypal_payment_page.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class KiindPaypalPayment extends StatelessWidget {
   final String clientId;
   final String amount;
   final String currencyCode;
-    final String returnURL;
+  final String returnURL;
   final String cancelURL;
   final bool sandboxMode;
   final String note;
@@ -21,8 +21,8 @@ class KiindPaypalPayment extends StatelessWidget {
     required this.amount,
     required this.currencyCode,
     required this.returnURL,
-  required this.cancelURL,
-  required this.sandboxMode,
+    required this.cancelURL,
+    required this.sandboxMode,
     required this.note,
     required this.secretKey,
     required this.onSuccess,
@@ -30,83 +30,36 @@ class KiindPaypalPayment extends StatelessWidget {
     required this.onCancel,
   }) : super(key: key);
 
+  // Method to launch PayPal URL in the system browser
+  Future<void> _launchPayPal() async {
+    // PayPal URL depending on sandboxMode
+    String paypalUrl =
+        sandboxMode
+            ? "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_xclick&business=$clientId&item_name=$note&amount=$amount&currency_code=$currencyCode&return=$returnURL&cancel_return=$cancelURL"
+            : "https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=$clientId&item_name=$note&amount=$amount&currency_code=$currencyCode&return=$returnURL&cancel_return=$cancelURL";
+
+    try {
+      if (await canLaunch(paypalUrl)) {
+        await launch(paypalUrl);
+      } else {
+        onError('Could not launch PayPal URL');
+      }
+    } catch (e) {
+      onError('Error launching PayPal URL: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    String paypalUrl =
-        "https://www.sandbox.paypal.com/sdk/js?client-id=$clientId&currency=$currencyCode";
-
-    String escapedNote = note.replaceAll("'", "\\'").replaceAll("\"", "\\\"");
-    String escapedAmount = amount.replaceAll("'", "\\'").replaceAll("\"", "\\\"");
-
-    String htmlContent = """
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <script src="$paypalUrl"></script>
-      </head>
-      <body>
-        <div id="paypal-button-container"></div>
-        <script>
-          paypal.Buttons({
-            createOrder: function(data, actions) {
-              return actions.order.create({
-                purchase_units: [{
-                  amount: {
-                    value: '$escapedAmount'
-                  },
-                  description: "$escapedNote"
-                }]
-              });
-            },
-            onApprove: function(data, actions) {
-              return actions.order.capture().then(function(details) {
-                window.onSuccess(JSON.stringify(details));
-              });
-            },
-            onCancel: function(data) {
-              window.onCancel();
-            },
-            onError: function(err) {
-              window.onError(err.toString());
-            }
-          }).render('#paypal-button-container');
-        </script>
-      </body>
-    </html>
-    """;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('PayPal Checkout'),
       ),
-      body: WebView(
-        initialUrl: Uri.dataFromString(htmlContent, mimeType: 'text/html').toString(),
-        javascriptMode: JavascriptMode.unrestricted,
-        javascriptChannels: {
-          JavascriptChannel(
-            name: 'onSuccess',
-            onMessageReceived: (message) {
-              try {
-                Map<String, dynamic> details = jsonDecode(message.message);
-                onSuccess(details);
-              } catch (e) {
-                onError('Invalid response: ${message.message}');
-              }
-            },
-          ),
-          JavascriptChannel(
-            name: 'onCancel',
-            onMessageReceived: (message) {
-              onCancel();
-            },
-          ),
-          JavascriptChannel(
-            name: 'onError',
-            onMessageReceived: (message) {
-              onError(message.message);
-            },
-          ),
-        },
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _launchPayPal,
+          child: const Text('Pay with PayPal'),
+        ),
       ),
     );
   }
